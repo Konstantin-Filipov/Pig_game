06dedfa13f057f98da8d70322f409c1576302cae
Konstantin-Filipov <kosio1234545@gmail.com>
2022-03-06 17:51:16 +0100

    adding main and shell.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Lets play "pig".
+
+Type "roll" in order to throw the dice to collect points,
+
+If you throw 1, your points get reset, thankfully you have the option
+to stop your turn and keep the collected points by typing the command
+"stop".
+
+Whoever reaches first 100 points, wins.
+"""
+
+import shell
+
+
+if __name__ == "__main__":
+    print(__doc__)
+    shell.Shell().cmdloop()
