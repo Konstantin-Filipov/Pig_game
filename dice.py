aeb002021f37e870cb7a3eee422c3a014ff15832
PaulPet993 <82890866+PaulPet993@users.noreply.github.com>
2022-03-09 23:05:48 +0100

    separating the dice graph into another method

--- dice.py
+++ dice.py
@@ -1,45 +1,48 @@
 import random
+
 class Dice:
-    faces = 6
     def __init__(self):
         random.seed()
 
     def roll(self):
         """Roll a dice once and return the value."""
-        roll = random.randint(1, self.faces)
-        if roll == 1:
+        roll = random.randint(1, 6)
+        return roll
+    
+    def dice_graph(self, r):
+        if r == 1:
             print(" _______")
             print("|       |")
             print("|   O   |")
             print("|_______|\n")
-            return roll
-        elif roll == 2:
+            
+        elif r == 2:
             print(" _______")
             print("| O     |")
             print("|       |")
             print("|_____O_|\n")
-            return roll
-        elif roll == 3:
+            
+        elif r == 3:
             print(" _______")
             print("| O     |")
             print("|   O   |")
             print("|_____O_|\n")
-            return roll
-        elif roll == 4:
+        
+        elif r == 4:
             print(" _______")
             print("| O   O |")
             print("|       |")
             print("|_O___O_|\n")
-            return roll
-        elif roll == 5:
+            
+        elif r == 5:
             print(" _______")
             print("| O   O |")
             print("|   O   |")
             print("|_O___O_|\n")
-            return roll
-        elif roll == 6:
+            
+        elif r == 6:
             print(" _______")
             print("| O   O |")
             print("| O   O |")
             print("|_O___O_|\n")
-            return roll
+            
