fc8665462583d77ad2fa9e14047c091f6f06ce8b
PaulPet993 <82890866+PaulPet993@users.noreply.github.com>
2022-03-09 09:39:40 -0800

    Graphical dice design

--- dice.py
+++ dice.py
@@ -7,4 +7,39 @@ class Dice:
     def roll(self):
         """Roll a dice once and return the value."""
         roll = random.randint(1, self.faces)
-        return roll
+        if roll == 1:
+            print(" _______")
+            print("|       |")
+            print("|   O   |")
+            print("|_______|\n")
+            return roll
+        elif roll == 2:
+            print(" _______")
+            print("| O     |")
+            print("|       |")
+            print("|_____O_|\n")
+            return roll
+        elif roll == 3:
+            print(" _______")
+            print("| O     |")
+            print("|   O   |")
+            print("|_____O_|\n")
+            return roll
+        elif roll == 4:
+            print(" _______")
+            print("| O   O |")
+            print("|       |")
+            print("|_O___O_|\n")
+            return roll
+        elif roll == 5:
+            print(" _______")
+            print("| O   O |")
+            print("|   O   |")
+            print("|_O___O_|\n")
+            return roll
+        elif roll == 6:
+            print(" _______")
+            print("| O   O |")
+            print("| O   O |")
+            print("|_O___O_|\n")
+            return roll
