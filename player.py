22bd49d86f5d57efea9856c2d47ea48a3ffb4e9d
Konstantin-Filipov <kosio1234545@gmail.com>
2022-03-11 11:02:58 +0100

    documentation

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+"""Player class"""
 class Player:
     score = 0
     score_turn = 0
@@ -6,15 +7,18 @@ class Player:
     index = 0
 
     def __init__(self, i):
+        """Constructor with 'index' parameter"""
         self.index = i
 
     def setName(self, updatedName):
-        #set current name of player
+        """"set current name of player"""
         self.name = updatedName
 
-    def update_score(self):
-       #This method should update the players score
-       self.score = self.score + self.score_turn
+    def update_score(self, toAdd):
+        """Update overall score"""
+        #This method should update the players score
+        self.score = self.score + toAdd
+
 
 
     #def sort(player_record_list):
