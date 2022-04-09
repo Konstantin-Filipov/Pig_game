3f10288d0cbe345ce8aaa184c2fbd8e37d742cd5
Konstantin-Filipov <kosio1234545@gmail.com>
2022-04-09 14:44:35 +0200

    player class updated

--- player.py
+++ player.py
@@ -8,19 +8,36 @@ class Player:
     player_counter = 1
     index = 0
     isCheater = False
+    isWiner = False
 
     def __init__(self, i):
         """Constructor with 'index' parameter"""
         self.index = i
 
-    def setName(self, updatedName):
+    def setName(self):
         """"set current name of player"""
-        self.name = updatedName
+        print(f"set player{self.index} name:")
+        self.name = input()
+        print(f"nickname for player{self.index} is set to {self.name}\n")
+        return self.name
 
     def update_score(self, toAdd):
-        """Update overall score"""
-        #This method should update the players score
+        """Update player's overall score"""
+        self.score_turn += toAdd
         self.score = self.score + toAdd
+        print(f"Player {self.name} rolled {toAdd}, overall score: {self.score}")
+
+    def isWinner(self):
+        if self.score >= 100:
+            print(f"\n{self.name} is the first one who reached 100 points!!")
+            print(f"{self.name} WON!\n")
+            self.isWiner = True
+            return True
+
+    def remove_points(self):
+        self.score -= self.score_turn
+        print(f"{self.name} rolled 1 and lost points from this turn, overall score is {self.score}")
+        return self.score
 
     def cheat(self):
         """roll dice until exceeds 100"""
@@ -31,14 +48,7 @@ class Player:
                 rolled += 1
             dice.Dice().dice_graph(rolled)
             self.update_score(rolled)
-            print(f"Player {self.name} rolled {rolled}, overall score: {self.score}")
 
         print("Congrats!!! You won by cheating :0!")
         self.isCheater = True
-
-
-
-    #def sort(player_record_list):
-        #this method should sort the players' scores.
-    #    for item in player_record_list:
-    #        item
+        return True
