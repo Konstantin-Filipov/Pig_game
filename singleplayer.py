58513291f3c55e5c654b95090b4e2a3ac9bbe077
Konstantin-Filipov <kosio1234545@gmail.com>
2022-04-13 23:49:26 +0200

    highscore list features to singleplayer games & unittest them

--- singleplayer.py
+++ singleplayer.py
@@ -5,6 +5,8 @@ import cmd
 import dice
 import player
 import bot
+import pickle
+
 
 class Singleplayer(cmd.Cmd):
     isprompt = True
@@ -25,11 +27,15 @@ class Singleplayer(cmd.Cmd):
         self.player1.setName()
 
     def do_hold(self, _):
-        """Type in "hold" to end your current turn"""
+        """Type in "hold" to end your current turn, then is bot turn"""
         self.player1.score_turn = 0 # reset the score turn for player 1
         print(f"Now is BOT's turn")
-        self.bot.bot_turn()#bot rolls the dice multiple times here
-        print (f"Now is {self.player1.name}'s turn")
+        self.bot.bot_turn()#call automatic bot turn
+
+        if self.bot.reached100(): # if True, end the game
+            self.do_exit(self)
+        else:
+            print (f"Now is {self.player1.name}'s turn")
 
     def do_cheat(self, _):
         """Type in "cheat" to win the game instantly"""
@@ -57,6 +63,25 @@ class Singleplayer(cmd.Cmd):
         self.player1.remove_points()
         self.do_hold(self)
 
+    def do_show_scores(self, _):
+        """type 'show_score' to show highscore table"""
+        with open ('singlepl_highscores.txt', 'r') as f:
+            data = f.read()
+            print(data)
+
+    def dumpHighScore(self):
+        """move player's scores and data to file"""
+        str0 = (f"mode: {self.bot.level}")
+        str1 = (f"name: {self.player1.name}, score: {self.player1.score}")
+        str2 = (f"name: {self.bot.name}, score: {self.bot.score}")
+        str3 = ("-----------------------")
+
+        with open('singlepl_highscores.txt', 'a+') as f:
+            f.write(str0 + "\n")
+            f.write(str1 + "\n")
+            f.write(str2 + "\n")
+            f.write(str3 + "\n")
+
     def do_start(self, _):
         """start a new game"""
         print("You have started a new game.\n")
@@ -69,7 +94,8 @@ class Singleplayer(cmd.Cmd):
 
     def do_exit(self, _):
         # pylint: disable=no-self-use
-        """Leave the game."""
+        """Leave the game and save scores to a file"""
+        self.dumpHighScore()
         print("press any key to leave game, press 's' to start a new one")
         comand = input()
         if comand == 's':
